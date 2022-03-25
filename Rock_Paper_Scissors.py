eec38c6b0a996a5cc8e6b0d598b87b9ba5841b3f
Kat <kemetcalf3.14@gmail.com>
2022-03-24 21:45:49 -0700

    Complete Python roshambo game

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,39 @@
+import random
+
+weapon = ["rock", "paper", "scissors"]
+
+player_name = input("What is your name?")
+result = f"I want to play a game, {player_name}."
+
+while True:
+    print(result)
+    my_weapon = input("Choose your weapon! Rock, paper, or scissors?")
+    my_weapon = my_weapon.lower()
+    comp_weapon = random.choice(weapon)
+
+    if comp_weapon == my_weapon:
+        result = "It's a tie!"
+        continue
+    elif comp_weapon == "rock" and my_weapon == "paper":
+        result = f"{player_name} wins!"
+        print(result)
+        break
+    elif comp_weapon == "rock" and my_weapon == "scissors":
+        result = "Computer wins!"
+        continue
+    elif comp_weapon == "paper" and my_weapon == "rock":
+        result = "Computer wins!"
+        continue
+    elif comp_weapon == "paper" and my_weapon == "scissors":
+        result = f"{player_name} wins!"
+        print(result)
+        break
+    elif comp_weapon == "scissors" and my_weapon == "rock":
+        result = f"{player_name} wins!"
+        print(result)
+        break
+    elif comp_weapon == "scissors" and my_weapon == "paper":
+        result = "Computer wins!"
+        continue
+    else:
+        result = "Weapon not allowed! Bad form!"
