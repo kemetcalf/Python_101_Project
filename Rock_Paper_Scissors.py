2e4a9bb679ced44004a3526712152ecd6a5a1b1f
Kat <kemetcalf3.14@gmail.com>
2022-03-24 22:08:37 -0700

    Added 'try again' prompt

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -12,17 +12,17 @@ while True:
     comp_weapon = random.choice(weapon)
 
     if comp_weapon == my_weapon:
-        result = "It's a tie!"
+        result = "It's a tie! Try again"
         continue
     elif comp_weapon == "rock" and my_weapon == "paper":
         result = f"{player_name} wins!"
         print(result)
         break
     elif comp_weapon == "rock" and my_weapon == "scissors":
-        result = "Computer wins!"
+        result = "Computer wins! Try again"
         continue
     elif comp_weapon == "paper" and my_weapon == "rock":
-        result = "Computer wins!"
+        result = "Computer wins! Try again"
         continue
     elif comp_weapon == "paper" and my_weapon == "scissors":
         result = f"{player_name} wins!"
@@ -33,7 +33,7 @@ while True:
         print(result)
         break
     elif comp_weapon == "scissors" and my_weapon == "paper":
-        result = "Computer wins!"
+        result = "Computer wins! Try again"
         continue
     else:
         result = "Weapon not allowed! Bad form!"
